f8e86dcb057fd90a5825508b7281dc3fc0de8359
jaideep272001 <83582355+jaideep272001@users.noreply.github.com>
2025-08-14 15:10:07 +0530

    Delete Frontend/src directory

--- Frontend/src/Components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-const Footer = ()=>{
-    return(
-    <small className="footer" >All Rights Reserved | © Jaitech Robotics and Automations Private Limited</small>
-    )
-}
-
-export default Footer;
--- Frontend/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import App from "./App";
-import { createRoot } from "react-dom/client";
-let app = createRoot(document.getElementById("root")).render(
-
-<App />
-
-)
